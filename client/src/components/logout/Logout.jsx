4cce7015cbddb4c145bcebca0bb19eae2ac2eb90
richmond-andoh <richmondandoh00@gmail.com>
2024-03-31 03:35:12 +0000

    Sidebar implementation

---
+++ client/src/components/logout/Logout.jsx
@@ -0,0 +1,19 @@
+import { RiLogoutCircleLine } from "react-icons/ri";
+// TODO Implement Logout functionality
+
+const Logout = () => {
+	return (
+		<>
+			<img
+				src={"https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"}
+				className='w-10 h-10 rounded-full border border-gray-800'
+			/>
+
+			<div className='cursor-pointer flex items-center p-2 rounded-lg bg-glass mt-auto border border-gray-800'>
+				<RiLogoutCircleLine size={23} />
+			</div>
+		</>
+	);
+};
+
+export default Logout;
