52203c7547f41a2090a2b360d76aeb59922bb57f
richmond-andoh <richmondandoh00@gmail.com>
2024-03-31 01:57:14 +0000

    scrollbar and background customization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-slate-900">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
