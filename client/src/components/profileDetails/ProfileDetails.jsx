3b0a21814787f0b5174b7a829a24d4383b5ed3c9
richmond-andoh <richmondandoh00@gmail.com>
2024-03-31 19:37:16 +0000

    user profile fetch successfull

--- client/src/components/profileDetails/ProfileDetails.jsx
+++ client/src/components/profileDetails/ProfileDetails.jsx
@@ -4,21 +4,21 @@ import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
 
-const ProfileDetails = () => {
-	const userProfile = {
-		avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-		bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-		email: "johndoe@gmail.com",
-		followers: 100,
-		following: 200,
-		html_url: "https://github.com/burakorkmez",
-		location: "Somewhere, Earth",
-		name: "John Doe",
-		public_gists: 100,
-		public_repos: 100,
-		twitter_username: "johndoe",
-		login: "johndoe",
-	};
+const ProfileDetails = ({ userProfile }) => {
+	// const userProfile = {
+	// 	avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+	// 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
+	// 	email: "johndoe@gmail.com",
+	// 	followers: 100,
+	// 	following: 200,
+	// 	html_url: "https://github.com/burakorkmez",
+	// 	location: "Somewhere, Earth",
+	// 	name: "John Doe",
+	// 	public_gists: 100,
+	// 	public_repos: 100,
+	// 	twitter_username: "johndoe",
+	// 	login: "johndoe",
+	// };
 
 	return (
 		<div className='lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10'>
@@ -29,11 +29,11 @@ const ProfileDetails = () => {
 					<a href={userProfile?.html_url} target='_blank' rel='noreferrer'>
 						<img src={userProfile?.avatar_url} className='rounded-md w-24 h-24 mb-2' alt='' />
 					</a>
-                    
+
 					{/* View on Github */}
 					<div className='flex gap-2 items-center flex-col'>
 						<a
-							href={userProfile.html_url}
+							href={userProfile?.html_url}
 							target='_blank'
 							rel='noreferrer'
 							className='bg-glass font-medium w-full text-xs p-2 rounded-md cursor-pointer border border-blue-400 flex items-center gap-2'
@@ -76,7 +76,7 @@ const ProfileDetails = () => {
 				{/* Member Since Date */}
 				<div className='my-2'>
 					<p className='text-gray-600 font-bold text-sm'>Member since</p>
-					<p className=''>21 Sep, 2023</p>
+					<p className=''>{userProfile?.created_at}</p>
 				</div>
 
 				{/* Email Address */}
