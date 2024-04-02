942c544c3a4df1613e97cf0e1e226f93692bbdbe
richmond-andoh <richmondandoh00@gmail.com>
2024-04-02 15:13:32 +0000

    some fixes in the sortRepos component

--- client/src/components/sortRepo/SortRepos.jsx
+++ client/src/components/sortRepo/SortRepos.jsx
@@ -3,21 +3,28 @@ const SortRepos = ({ sortHandler, sortType }) => {
 		<div className='mb-2 flex justify-center lg:justify-center'>
 			<button
 				type='button' onClick={() => sortHandler('recent')}
-				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${sortType === "recent" ? "border-green-500" : ""}`}
 			>
 				Most Recent
 			</button>
 			<button
-				type='button' onClick={() => sortHandler("starts")}
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				type='button' onClick={() => sortHandler("stars")}
+				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${sortType === "stars" ? "border-green-500" : ""}`}
 			>
 				Most Stars
 			</button>
 			<button
 				type='button' onClick={() => sortHandler("forks")}
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${sortType === "forks" ? "border-green-500" : ""}`}
 			>
 				Most Forks
+			</button>
+
+			<button
+				type='button' onClick={() => sortHandler("forks")}
+				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+			>
+				Most Commit Message
 			</button>
 		</div>
 	);
