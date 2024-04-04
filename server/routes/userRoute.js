8cb1af860a91899d660c55bf21b0ffc815b0e949
richmond-andoh <richmondandoh00@gmail.com>
2024-04-04 18:01:01 +0000

    user route file added

---
+++ server/routes/userRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const userRoute = express.Router();
+
+// @route   GET api/users
+
+
+export default userRoute;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
-
+import userRoute from "./routes/userRoute.js";
 const app = express();
 
 
 app.get( "/", ( req, res ) => {
     res.send("Hello, Server is ready")
 });
+
+app.use("api/userss",  userRoute);
 
 app.listen(5050, () => {
     console.log("Server started perfectly and ready to go");
