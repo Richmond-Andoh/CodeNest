6f2a3a36f255ed72a9dacd265b6963e84863c4a2
richmond-andoh <richmondandoh00@gmail.com>
2024-04-04 15:54:01 +0000

    svg icons added

--- client/src/pages/explore/Explore.jsx
+++ client/src/pages/explore/Explore.jsx
@@ -19,9 +19,9 @@ const Explore = () => {
 		setrepos(data.items);
 
 	   } catch (error) {
-		toast.error("error.message")
+		toast.error("error.message");
 		} finally{
-		setloading(false)
+		setloading(false);
 	   }
 	}
 	
@@ -31,15 +31,15 @@ const Explore = () => {
 				<h1 className='text-xl font-bold text-center'>Explore Popular Repositories</h1>
 				<div className='flex flex-wrap gap-2 my-2 justify-center'>
 					<img src='/javascript.svg' onClick={() => handleExploreLanguages("javascript")} alt='JavaScript' className='h-11 sm:h-20 cursor-pointer' />
-					<img src='/typescript.svg' alt='TypeScript logo' className='h-11 sm:h-20 cursor-pointer' />
-					<img src='/c++.svg' alt='C++ logo' className='h-11 sm:h-20 cursor-pointer' />
-					<img src='/python.svg' alt='Python logo' className='h-11 sm:h-20 cursor-pointer' />
-					<img src='/java.svg' alt='Java logo' className='h-11 sm:h-20 cursor-pointer' />
+					<img src='/typescript.svg' alt='TypeScript logo' onClick={() => handleExploreLanguages("typescript")} className='h-11 sm:h-20 cursor-pointer' />
+					<img src='/php.svg' alt='PHP logo' onClick={() => handleExploreLanguages("php")} className='h-11 sm:h-20 cursor-pointer' />
+					<img src='/python.svg' alt='Python logo' onClick={() => handleExploreLanguages("python")} className='h-11 sm:h-20 cursor-pointer' />
+					<img src='/java.svg' alt='Java logo' onClick={() => handleExploreLanguages("java")} className='h-11 sm:h-20 cursor-pointer' />
 				</div>
 
 				{ repos.length > 0 && (
 					<h2 className='text-lg font-semibold text-center my-4'>
-						<span className='bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded-full '>
+						<span className='bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded-full'>
 							{selectedLanguage.toUpperCase()}{" "}
 						</span>
 						Repositories
