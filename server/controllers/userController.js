5470c51fc5cd72429b7a85bc793e05d16f2867a3
richmond-andoh <richmondandoh00@gmail.com>
2024-04-09 13:55:52 +0000

    Implementing like profile page

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+import User from "../modules/User.js";
+
 export const getUserProfileAndRepos = async(req, res) => {
 
     const { username } = req.params;
@@ -27,3 +29,48 @@ export const getUserProfileAndRepos = async(req, res) => {
         res.status(500).json({ error: error.message});
     }
 }
+
+export const likeProfile = async (req, res) => {
+    try {
+        
+        const { username } = req.params;
+        const user = await User.findById(req.user._id.toString());
+        console.log(user);
+
+        const userToLike = await User.findOne({ username });
+        if (!user || !userToLike) return res.status(404).json('User not found');
+
+        if(user.likedProfiles.includes(userToLike.username)) return res.status(404).json({ message: 'Already liked this profile' });
+    //     let alreadyLiked = false;
+    //     for (let i=0; i < user.likes.length; i++) {
+    //        if (user.likes[i].equals(userToLike._id)) {
+    //            alreadyLiked = true; 
+    //            break;
+    //        }
+    //    };
+
+        userToLike.likedBy.push({ username: user.username, avatarUrl: user.avatarUrl, likeDate: Date.now()});
+        user.likedProfiles.push(userToLike.username);
+
+        await userToLike.save();
+        await user.save();
+
+        await Promise.all([
+            user.save(),
+            userToLike.save()
+        ]);
+
+        res.status(201).json({ message: 'Successfully liked profile' })
+    } catch (error) {
+        return res.status(500).json({ Error: error.message})
+    };
+};
+
+export const getLikes = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id.toString());
+        return res.status(200).json({ likedBy: user.likedBy })
+    } catch (error) {
+        return res.status(500).json({ Error: error.message});
+    };
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { getUserProfileAndRepos } from "../controllers/userController.js";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
+import { getLikes, likeProfile } from "../controllers/userController.js";
 
 const userRoute = express.Router();
 
 // @route   GET api/users
-userRoute.get("/profile/:username", getUserProfileAndRepos)
+userRoute.get("/profile/:username", getUserProfileAndRepos);
+userRoute.get("/likes", ensureAuthenticated, getLikes);
+userRoute.get("/likes/:username", ensureAuthenticated, likeProfile)
 
 
 export default userRoute;
